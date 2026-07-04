699e7635c2a08bad5caf34f0b261c79bcabc8582
Krishna Mani Teja Katragadda <63157498+KrishnaKatragadda@users.noreply.github.com>
2026-07-04 11:50:29 -0700

    Add: valid-perfect-square - submission-0

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 1
+        right = num ## things are sorted and can apply BS
+
+        while left<=right:
+            mid = (left+right)//2
+
+            if mid*mid>num:
+                right = mid-1
+            elif mid*mid<num:
+                left = mid+1
+            else:
+                return True
+        
+        return False
+        
