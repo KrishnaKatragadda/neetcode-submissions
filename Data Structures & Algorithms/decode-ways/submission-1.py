9c8ace0aaca7304114a65526cd72fd1414e178f4
Krishna Mani Teja Katragadda <63157498+KrishnaKatragadda@users.noreply.github.com>
2026-09-08 14:58:38 -0700

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        def dfs(i):
+
+            ## if the current index is past string length
+            ## you have found a valid decoding
+
+            if i == len(s):
+                return 1
+            
+            if s[i]=="0":
+                return 0
+            if i in memo:
+                return memo[i]
+            
+            res = dfs(i+1)
+
+            if i+1 < len(s) and 10<= int(s[i:i+2]) <27:
+                res+=dfs(i+2)
+            
+            memo[i]= res
+            
+            return res
+        
+        return dfs(0)
+        
