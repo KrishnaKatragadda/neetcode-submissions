17cb00569a03c38f4e34eccfa0b297cd37340ec3
Krishna Mani Teja Katragadda <63157498+KrishnaKatragadda@users.noreply.github.com>
2026-09-11 13:22:41 -0700

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.py
@@ -0,0 +1,37 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # n = len(s)
+        # memo = {}
+        # def dfs(i):
+
+        #     if i>=n: ## if the current iteration cross the length of the string
+        #     ## we have reached the limit without finding any issue.
+        #         return True
+            
+        #     if i in memo: ## if the position is already explored return that 
+        #         return memo[i]
+        #     for w in wordDict:
+        #         l = len(w)
+        #         if s[i:i+l] == w:
+        #             if dfs(i+l): 
+        #                 memo[i] = True
+        #                 return True
+        #     memo[i]=False
+        #     return False
+        
+        # return dfs(0)
+        
+        ## trying our Bottom-Up approach
+        n = len(s)
+        dp = [False]*(n+1)
+        dp[0] = True ## for starting of the string which is NULL, we dont need anything so True
+        for i in range(n):
+            if not dp[i]: continue
+            for w in wordDict:
+                l = len(w)
+                if s[i:i+l]==w:
+                    dp[i+l]=True
+        
+        return dp[n]
+
+        
